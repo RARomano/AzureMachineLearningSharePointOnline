b8bb8eb7c35a0bd9d4b2b782541068ef04b70fb5
RARomano <teste>
2015-05-22 19:52:07 -0300

    Final solution

---
+++ SPSamples.RemoteEventReceiver/Features/Feature1/Feature1.feature
@@ -0,0 +1,2 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="83e71fae-c873-4419-a310-a0e3a4c9613a" featureId="83e71fae-c873-4419-a310-a0e3a4c9613a" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SPSamples.RemoteEventReceiver Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel" />
---
+++ SPSamples.RemoteEventReceiverWeb/Services/AppEventReceiver.svc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+using System.ServiceModel;
+
+namespace SPSamples.RemoteEventReceiverWeb.Services
+{
+    public class AppEventReceiver : IRemoteEventService
+    {
+        /// <summary>
+        /// Handles app events that occur after the app is installed or upgraded, or when app is being uninstalled.
+        /// </summary>
+        /// <param name="properties">Holds information about the app event.</param>
+        /// <returns>Holds information returned from the app event.</returns>
+        public SPRemoteEventResult ProcessEvent(SPRemoteEventProperties properties)
+        {
+            SPRemoteEventResult result = new SPRemoteEventResult();
+
+
+
+            switch (properties.EventType)
+            {
+                case SPRemoteEventType.AppInstalled:
+                    HandleAppInstalled(properties);
+                    break;
+
+                case SPRemoteEventType.ItemAdded:
+                    HandleItemAdded(properties);
+                    break;
+
+            }
+            return result;
+
+
+        }
+
+        private void HandleItemAdded(SPRemoteEventProperties properties)
+        {
+            using (var clientContext =
+                  TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+            {
+                var web = clientContext.Web;
+                var list = web.Lists.GetById(properties.ItemEventProperties.ListId);
+                var item = list.GetItemById(properties.ItemEventProperties.ListItemId);
+                item["Categoria"] = "TESTE";
+                item.Update();
+                clientContext.ExecuteQuery();
+            }
+        }
+
+        private void HandleAppInstalled(SPRemoteEventProperties properties)
+        {
+            using (var clientContext =
+                   TokenHelper.CreateAppEventClientContext(properties, false))
+            {
+                var myList = clientContext.Web.Lists.GetByTitle("Pictures");
+                clientContext.Load(myList);
+                clientContext.ExecuteQuery();
+
+                var receiver =
+                    new EventReceiverDefinitionCreationInformation
+                    {
+                        EventType = EventReceiverType.ItemAdded
+                    };
+
+                //Get WCF URL where this message was handled                
+                var msg = OperationContext.Current.RequestContext.RequestMessage;
+
+                receiver.ReceiverUrl = msg.Headers.To.ToString();
+
+                receiver.ReceiverName = "ItemAdded";
+                receiver.Synchronization = EventReceiverSynchronization.Synchronous;
+                myList.EventReceivers.Add(receiver);
+
+                clientContext.ExecuteQuery();
+
+            }
+        }
+
+        /// <summary>
+        /// This method is a required placeholder, but is not used by app events.
+        /// </summary>
+        /// <param name="properties">Unused.</param>
+        public void ProcessOneWayEvent(SPRemoteEventProperties properties)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
